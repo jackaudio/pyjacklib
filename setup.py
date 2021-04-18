9a86be02888496e116978480841098c3b78928c5
Christopher Arndt <chris@chrisarndt.de>
2021-04-18 21:55:48 +0200

    Add basic Python packaging setup
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Setup file for pyjacklib."""
+
+from os.path import dirname, join
+
+from setuptools import setup
+
+
+def read(*args):
+    return open(join(dirname(__file__), *args)).read()
+
+
+PKGDIR = 'jacklib'
+version = {}
+exec(read(PKGDIR, 'version.py'), {}, version)
+setup(packages=[PKGDIR], **version)
