e2b69701071502dc58cc7aeddc6621c5d9422a8c
Christopher Arndt <chris@chrisarndt.de>
2021-04-18 23:08:09 +0200

    Restructure package to keep imports short
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

--- jacklib/helpers.py
+++ jacklib/helpers.py
@@ -22,7 +22,7 @@
 
 from __future__ import absolute_import, print_function, unicode_literals
 
-from . import jacklib
+from . import api as jacklib
 
 
 # -------------------------------------------------------------------------------------------------
