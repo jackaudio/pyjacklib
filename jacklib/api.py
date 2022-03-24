009df2a39dae638768df601cad77d93bc0a367c7
Christopher Arndt <chris@chrisarndt.de>
2022-03-24 13:01:10 +0100

    fix: remove superfluous 'from __future__'  imports
    Python 2 is not supported
    
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

--- jacklib/api.py
+++ jacklib/api.py
@@ -18,8 +18,6 @@
 # -------------------------------------------------------------------------------------------------
 # Imports (Global)
 
-from __future__ import absolute_import, print_function, unicode_literals
-
 from collections import namedtuple
 from ctypes import (
     ARRAY,
